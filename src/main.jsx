6f60d8022c9ea0266c03b961cb3918982725457e
Gugzz21 <gustavodinizz966@gmail.com>
2026-02-24 17:48:16 -0300

    Primeiro Commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// Imports -> Estão importando alguma variável, função ou classe de outros arquivos.
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
