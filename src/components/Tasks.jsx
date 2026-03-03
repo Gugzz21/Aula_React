98791729e07a56b2057b3730ab6ed3a77d2821de
Gugzz21 <gustavodinizz966@gmail.com>
2026-03-03 13:56:48 -0300

    Criando mais componentes para evitar duplicidade de código

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,6 @@
 import { ChevronRight, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   const navigate = useNavigate();
@@ -23,19 +24,19 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
               {task.title}
             </button>
 
-            <button
+            <Button
               onClick={() => onSeeDetailsClick(task)}
               className="bg-slate-400 p-2 rounded-md text-white"
             >
               <ChevronRight />
-            </button>
+            </Button>
 
-            <button
+            <Button
               onClick={() => onDeleteTaskClick(task.id)}
               className="bg-slate-400 p-2 rounded-md text-white"
             >
               <TrashIcon />
-            </button>
+            </Button>
           </li>
         ))}
       </ul>
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon } from "lucide-react";
 import { useSearchParams, useNavigate } from "react-router-dom";
+import Title from "../components/Title";
 
 function TaskPage() {
   const navigate = useNavigate();
@@ -17,9 +18,8 @@ function TaskPage() {
           >
             <ChevronLeftIcon />
           </button>
-          <h1 className="text-3xl text-slate-100 font-bold text-center">
-            Detalhes da Tarefa
-          </h1>
+
+          <Title>Detalhes da Tarefa</Title>
         </div>
 
         <div className="bg-slate-200 p-4 rounded-md">
