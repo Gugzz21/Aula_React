192052d9e64ff34037f99a7aebe991b29234f570
Gugzz21 <gustavodinizz966@gmail.com>
2026-03-03 00:57:55 -0300

    Página de detalhes da tarefa + botão de retornar para página principal

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,39 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  console.log(title, description);
   return (
-    <div className=" my-4 space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <input
         type="text"
         placeholder="Digite o título da tarefa"
-        className=" bg-white border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
-      ></input>
+        className="bg-white border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
       <input
         type="text"
         placeholder="Digite a descrição da tarefa"
         className="bg-white border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
-      ></input>
-      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          // Verificar se o título e a descrição estão preenchidos
+          if (!title.trim() || !description.trim()) {
+            return alert(
+              "Por favor, preencha o título e a descrição da tarefa.",
+            );
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
         Adicionar
       </button>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,16 @@
-import { ChevronRight, DeleteIcon, TrashIcon } from "lucide-react";
+import { ChevronRight, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
-  console.log(tasks);
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
+
   return (
     <div>
       <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
@@ -14,7 +23,10 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
               {task.title}
             </button>
 
-            <button className="bg-slate-400 p-2 rounded-md text-white">
+            <button
+              onClick={() => onSeeDetailsClick(task)}
+              className="bg-slate-400 p-2 rounded-md text-white"
+            >
               <ChevronRight />
             </button>
 
